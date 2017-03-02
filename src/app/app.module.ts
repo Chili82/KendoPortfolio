b0c276b5f7dffb4381982ab4836d13869fa2cb54
Chili82 <ilicbor@gmail.com>
2017-03-02 19:48:51 +0100

    commit from Visual Studio Code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
+
+// Meni componente
 import { PortfolioComponent } from './portfolio.component';
 import { GrafikonComponent } from './grafikon.component';
+
+// Kendo UI componente
 import { ButtonsModule } from '@progress/kendo-angular-buttons';
 import { TabStripModule } from '@progress/kendo-angular-layout';
 import { ChartsModule } from '@progress/kendo-angular-charts';
--- src/app/grafikon.component.ts
+++ src/app/grafikon.component.ts
@@ -2,53 +2,23 @@ import { Component } from '@angular/core';
 
 
 @Component({
-    template: `<h1>{{title}}</h1>
-    <div style="width:100%">
-    <kendo-tabstrip>
-        <kendo-tabstrip-tab [title]="'Tab 1 Title'" [selected]="true">
-          <template kendoTabContent>
-            <kendo-chart [categoryAxis]="{ categories: categories }">
-            <kendo-chart-title text="Gross domestic product growth /GDP annual %/"></kendo-chart-title>
-            <kendo-chart-legend position="bottom" orientation="horizontal"></kendo-chart-legend>
-            <kendo-chart-tooltip format="{0}%"></kendo-chart-tooltip>
-            <kendo-chart-series>
-                <kendo-chart-series-item *ngFor="let item of series"
-                    type="line" style="smooth" [data]="item.data" [name]="item.name">
-                </kendo-chart-series-item>
-            </kendo-chart-series>
-        </kendo-chart>
-          </template>
-        </kendo-tabstrip-tab>
-        <kendo-tabstrip-tab [title]="'Tab 2 Title'">
-          <template kendoTabContent>
-            <p>Tab 2 Content</p>
-          </template>
-        </kendo-tabstrip-tab>
-        <kendo-tabstrip-tab [title]="'Tab 3 Title'">
-          <template kendoTabContent>
-            <p>Tab 3 Content</p>
-          </template>
-        </kendo-tabstrip-tab>
-      </kendo-tabstrip>
-      </div>
-    <button kendoButton (click)="onButtonClick()" [primary]="true">My Kendo UI Button</button>`
-   
+   templateUrl: './grafikon.component.html',
 })
 
 export class GrafikonComponent {
     title = 'Hello World!';
 
     private series: any[] = [{
-    name: "India",
+    name: 'India',
     data: [3.907, 7.943, 7.848, 9.284, 9.263, 9.801, 3.890, 8.238, 9.552, 6.855]
   }, {
-    name: "Russian Federation",
+    name: 'Russian Federation',
     data: [4.743, 7.295, 7.175, 6.376, 8.153, 8.535, 5.247, -7.832, 4.3, 4.3]
   }, {
-    name: "Germany",
+    name: 'Germany',
     data: [0.010, -0.375, 1.161, 0.684, 3.7, 3.269, 1.083, -5.127, 3.690, 2.995]
   },{
-    name: "World",
+    name: 'World',
     data: [1.988, 2.733, 3.994, 3.464, 4.001, 3.939, 1.333, -2.245, 4.339, 2.727]
   }];
   private categories: number[] = [2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011];
