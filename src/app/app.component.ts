f1f7af55ba528ad382763c39eefec14e7b4d1f00
Boris Ilic <ilicbor@gmail.com>
2017-03-03 14:25:40 +0100

    g

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [
-        // load the default theme (use correct path to node_modules)
-        '../../node_modules/@progress/kendo-theme-default/dist/all.css'
-    ],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Hello!';
