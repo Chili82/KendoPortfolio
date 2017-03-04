58268ddb517d9727e2eb3149328a8fed3440a8f0
Chili82 <ilicbor@gmail.com>
2017-03-04 17:00:50 +0100

    'ff'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import '../../node_modules/@progress/kendo-theme-default/dist/all.css';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['../../node_modules/@progress/kendo-theme-default/dist/all.css']
 })
 export class AppComponent {
   title = 'Hello!';
