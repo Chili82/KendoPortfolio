73f7d6342de6b8a7a3aab2c6f73d5098ed396562
Boris Ilic <ilicbor@gmail.com>
2017-03-06 23:29:17 +0100

    commit from vsc

--- src/app/portfolio.service.ts
+++ src/app/portfolio.service.ts
@@ -1,11 +1,11 @@
 import {Portfolio} from './portfolio';
 
 
-export var productList: Portfolio[] = [
-  {"security": "EQ-INTERNATIONAL", "ISIN": "GB00065658", "position": 5958,"closePrice":49.66,"livePrice":89.78},
-  {"security": "EQ-DEUTSCHE TELEKOM", "ISIN": "DE000578315", "position": 2206,"closePrice":40.949,"livePrice":80.64}
-  ,{"security": "EQ-NEXANAS", "ISIN": "FR0034303", "position": -2245,"closePrice":38.53,"livePrice":50.94},
-  {"security": "CB-PARPUBLICA", "ISIN": "PTPE000578315", "position": 2206,"closePrice":80.949,"livePrice":560.64},
-  {"security": "EQ-GALP ENERGIA", "ISIN": "DE000578315", "position": 2206,"closePrice":62.3,"livePrice":38.59},
-  {"security": "EQ-PARGESA HOLDING", "ISIN": "DE000578315", "position": 2206,"closePrice":20.949,"livePrice":60.84}
+export let productList: Portfolio[] = [
+  {'security': 'EQ-INTERNATIONAL', 'ISIN': 'GB00065658', 'position': 5958, 'closePrice':  49.66, 'livePrice': 89.78},
+  {'security': 'EQ-DEUTSCHE TELEKOM', 'ISIN': 'DE000578315', 'position': 2206, 'closePrice': 40.949, 'livePrice': 80.64}
+  , {'security': 'EQ-NEXANAS', 'ISIN': 'FR0034303', 'position': -2245, 'closePrice': 38.53, 'livePrice': 50.94},
+  {'security': 'CB-PARPUBLICA', 'ISIN': 'PTPE000578315', 'position': 2206, 'closePrice': 80.949, 'livePrice': 560.64},
+  {'security': 'EQ-GALP ENERGIA', 'ISIN': 'DE000578315', 'position': 2206, 'closePrice': 62.3, 'livePrice': 38.59},
+  {'security': 'EQ-PARGESA HOLDING', 'ISIN': 'DE000578315', 'position': 2206, 'closePrice': 20.949, 'livePrice': 60.84}
 ]
